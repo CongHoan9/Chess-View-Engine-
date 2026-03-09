3d62c84e6873a1646c3e7a398e727d6cd68dbd46
CongHoan9 <158456623+CongHoan9@users.noreply.github.com>
2026-03-09 11:52:28 +0700

    Add files via upload

---
+++ Chess/Chess/Extensions/PRNG.cs
@@ -0,0 +1,18 @@
+﻿namespace Chess
+{
+    public sealed class PRNG(ulong seed)
+    {
+        private ulong seed = seed;
+        public ulong Rand64()
+        {
+            seed ^= seed >> 12;
+            seed ^= seed << 25;
+            seed ^= seed >> 27;
+            return seed * 2685821657736338717UL;
+        }
+        public ulong SparseRand()
+        {
+            return Rand64() & Rand64() & Rand64();
+        }
+    }
+}
